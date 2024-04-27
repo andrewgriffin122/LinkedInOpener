4683e84b6c12539e2b12781eafcddb08b2b9edb6
Andrew Griffin <andrew.griffin@mymail.champlain.edu>
2024-04-27 01:11:26 -0400

    now using shell api stuff

--- LinkedInOpener/LinkedInOpener/LinkedInOpener.cpp
+++ LinkedInOpener/LinkedInOpener/LinkedInOpener.cpp
@@ -1,29 +1,42 @@
 #include <Windows.h>
 #include <iostream>
+#include <shellapi.h>
+#include <cstdlib>
+#include <ctime>
 
 //https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-keybd_event
 //https://learn.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
-int main() {
+
+const char* websites[4] = {
+    "https://www.linkedin.com/in/scbarrett/",
+    "https://www.farmersonly.com/",
+    "https://www.twitch.tv/moistcr1tikal",
+    "https://www.youtube.com/watch?v=lYBUbBu4W08"
+};
+int main() 
+{
+    srand(static_cast<unsigned int>(time(0)));
     while (true)
     {
         //Press A
         if (GetAsyncKeyState(0x41))
         {
-            std::cout << "Key Pressed\n";
-            Sleep(10);
-            system("CLS");
-            keybd_event(VK_LSHIFT, 0, 0, 0);
-            keybd_event(VK_CONTROL, 0, 0, 0);
-            keybd_event(VK_LWIN, 0, 0, 0);
-            keybd_event(VK_LMENU, 0, 0, 0);
-            keybd_event(0x4C, 0, 0, 0);
-            //Sleep(1);
-            keybd_event(0x4C, 0, KEYEVENTF_KEYUP, 0);
-            keybd_event(VK_LMENU, 0, KEYEVENTF_KEYUP, 0);
-            keybd_event(VK_LWIN, 0, KEYEVENTF_KEYUP, 0);
-            keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYUP, 0);
-            keybd_event(VK_LSHIFT, 0, KEYEVENTF_KEYUP, 0);
-            Sleep(5000);
+            ShellExecuteA(0, 0, websites[rand() % 4], 0, 0, SW_SHOW);
+            //std::cout << "Key Pressed\n";
+            Sleep(1000);
+            //system("CLS");
+            //keybd_event(VK_LSHIFT, 0, 0, 0);
+            //keybd_event(VK_CONTROL, 0, 0, 0);
+            //keybd_event(VK_LWIN, 0, 0, 0);
+            //keybd_event(VK_LMENU, 0, 0, 0);
+            //keybd_event(0x4C, 0, 0, 0);
+            ////Sleep(1);
+            //keybd_event(0x4C, 0, KEYEVENTF_KEYUP, 0);
+            //keybd_event(VK_LMENU, 0, KEYEVENTF_KEYUP, 0);
+            //keybd_event(VK_LWIN, 0, KEYEVENTF_KEYUP, 0);
+            //keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYUP, 0);
+            //keybd_event(VK_LSHIFT, 0, KEYEVENTF_KEYUP, 0);
+            //Sleep(5000);
         }
 
     }
